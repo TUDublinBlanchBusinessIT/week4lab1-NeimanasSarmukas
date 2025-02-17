77b77b9affd83a3cefbb26009fe7751d3c20b165
B00156641 Neimanas Sarmukas <B00156641@mytudublin.ie>
2025-02-17 16:24:55 +0000

    commit 4

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -17,5 +17,19 @@ class CustomerController extends Controller
         $customer->setSurname($request->surname);//sets the attributes
         $customer->save();//persists the Customer object to the database
     } 
+
+    public function edit($id)
+    {
+         $customer = Customer::find($id);
+         return view('customers.edit')->with('customer', $customer);
+    }
+
+    public function update(Request $request)
+    {
+        $customer = Customer::find($request->id);
+        $customer->setFirstname($request->firstname);
+        $customer->setSurname($request->surname);
+        $customer->save();
+    }
 } 
 ?> 
