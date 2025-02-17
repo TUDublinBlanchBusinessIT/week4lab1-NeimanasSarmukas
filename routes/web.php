53fb30cadd021f5021b284eb0f0563d1c63b4c10
B00156641 Neimanas Sarmukas <B00156641@mytudublin.ie>
2025-02-17 15:26:08 +0000

    second commit

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+    
 });
+
+Route::get('/customers/new', 'App\Http\Controllers\CustomerController@new');
+Route::post('/customers/create', 'App\Http\Controllers\CustomerController@create')->name('customers.create'); 
